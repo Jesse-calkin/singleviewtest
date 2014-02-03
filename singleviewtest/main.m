7754e3bbc8b4ee812b7c4bc7cb46abfb61d6f8e1
Jesse Calkin <jesse@Jesses-iMac.local>
2014-02-03 14:23:24 -0700

    updated class prefix

--- singleviewtest/main.m
+++ singleviewtest/main.m
@@ -8,11 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-#import "SPLAppDelegate.h"
+#import "SVTAppDelegate.h"
 
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SPLAppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SVTAppDelegate class]));
     }
 }
